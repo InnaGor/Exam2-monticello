dab477af163132860374875f4d84117dbe7778a4
inna_horbenko <inna.gorbenko@gmail.com>
2019-12-09 16:13:18 +0200

    slider1 is added

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(() => {
+    $('.slider1').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        fade: true,
+        dots: true,
+        dotsClass: "vertical-dots",
+    });
+});
