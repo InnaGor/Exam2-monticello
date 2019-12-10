51e1704358a7d0af9e1caccf7748c51f4a5cebad
inna_horbenko <inna.gorbenko@gmail.com>
2019-12-10 18:59:26 +0200

    section news is added

--- js/slider.js
+++ js/slider.js
@@ -8,4 +8,11 @@ $(() => {
         dots: true,
         dotsClass: "vertical-dots",
     });
+    $('.slider2').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+    });
 });
