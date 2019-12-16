551bfd8609f1cc636cd1b6f66a63aae9f6be23dd
inna_horbenko <inna.gorbenko@gmail.com>
2019-12-16 19:59:19 +0200

    footer, map

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $(() => {
         dots: true,
         dotsClass: "vertical-dots",
     });
+
+    // slider News
     $('.slider2').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -16,3 +18,13 @@ $(() => {
         dots: true,
     });
 });
+
+function initMap() {
+    // The location of office
+    let place = { lat: 40.678332, lng: -73.898346 };
+    // The map, centered at the office
+    let map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 13, center: place });
+    // The marker, positioned at the office
+    let marker = new google.maps.Marker({ position: place, map: map });
+}
